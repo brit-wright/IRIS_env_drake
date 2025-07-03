e7d791fff2c1ea6de7a6b335c56b8c81f7c53ff4
brit-wright <87948158+brit-wright@users.noreply.github.com>
2025-07-03 16:27:47 -0700

    Updated with correct Dijkstra's algorithm

--- map_building_using_polytope_intersections_mattress_problem.py
+++ map_building_using_polytope_intersections_mattress_problem.py
@@ -10,9 +10,11 @@ from pydrake.all import *#!/usr/bin/env python3.8
 import numpy as np
 import matplotlib.pyplot as plt
 
-from math import sqrt
+from math import sqrt, inf
 
 import shapely
+
+import bisect
 
 import random
 
@@ -158,7 +160,7 @@ sample_pts = []
 
 # let's do 3 sample points
 
-num_samples = 100
+num_samples = 200
 
 for pt in range(num_samples):
     sample_pt = np.array([np.random.uniform(x1_min, x1_max), np.random.uniform(x2_min, x2_max)])
@@ -230,7 +232,7 @@ def create_coords(x1, y1, x2, y2):
 
     if x1 != x2:
 
-        for x_point in np.arange(xmin, xmax, 0.25):
+        for x_point in np.arange(xmin, xmax, 0.1):
             y_point = y_xmin + (y_xmax - y_xmin)/(xmax - xmin) * (x_point - xmin)
             coord_list.append((float(x_point), float(y_point)))
 
@@ -239,7 +241,7 @@ def create_coords(x1, y1, x2, y2):
         y_min = min(y1, y2)
         y_max = max(y1, y2)
 
-        for y_point in np.arange(y_min, y_max, 0.25):
+        for y_point in np.arange(y_min, y_max, 0.1):
             coord_list.append((x1, float(y_point)))
     # print(coord_list)
     return coord_list
@@ -342,15 +344,136 @@ for inter_el in inters_list:
 
 # Requirement 1: Length of the polytope pair list = length of intersection list = length of polytope pair centers list = length of intersection center list
 
-print(f'Length of polytope pair list: {len(refined_polytope_pairs)}')
-print(f'Length of center pairs list: {len(refined_center_pairs)}')
-print(f'Length of intersection list: {len(refined_inters_list)}')
-print(f'Length of intersection centers list: {len(refined_inters_centers)}')
+# print(f'Length of polytope pair list: {len(refined_polytope_pairs)}')
+# print(f'Length of center pairs list: {len(refined_center_pairs)}')
+# print(f'Length of intersection list: {len(refined_inters_list)}')
+# print(f'Length of intersection centers list: {len(refined_inters_centers)}')
 
 # Since this requirement has been passed, we can go into drawing the tree
 
 # This is done in the plotting section where basically, for every pair of center points, there's
 # a corresponding intersection center. So we do a plt.plot to connect the points
+
+# So basically need to update my implementation. Each polytope and polytope intersection is supposed to be its own node lol.
+# Each node will also have its own list of neighbours. Parents are initialized to nothing. 
+
+
+# need to make a mega list in order to convert all the current intersections into nodes as well
+# and to label the neighbours
+
+class Node:
+
+    def __init__(self, polytope, coords):
+
+        self.polytope = polytope
+        self.coords = coords
+        self.neighbours = []
+
+        self.parent = None
+        self.cost = inf
+
+        self.seen = False
+        self.done = False
+
+    
+    def edge_cost(self, other):
+        dist = sqrt((self.coords[0] - other.coords[0])**2 + (self.coords[1] - other.coords[1])**2)
+        return dist 
+        
+    
+    # print the node for debugging
+    def __str__(self):
+        return self.coords
+
+    # Define the "less-than" to enable sorting by cost.
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+
+# first define every coordinate as a node object
+
+# create the mega-list of all polytopes and all coords
+
+mega_topes = []
+mega_coords = []
+
+for element in refined_center_pairs:
+    for coord in element:
+        mega_coords.append(coord)
+        mega_topes.append(refined_polytope_pairs[refined_center_pairs.index(element)][element.index(coord)])
+
+mega_coords = mega_coords + refined_inters_centers
+mega_topes = mega_topes + refined_inters_list
+
+
+already_processed_nodes = [] # has the coordinated of the nodes that have already been processed
+nodes = []
+
+# build a node list containing only the polytope and the coordinates of the center
+for element_idx in range(len(mega_coords)):
+
+    if mega_coords[element_idx] not in already_processed_nodes:
+        node_poly = mega_topes[element_idx]
+        node_coord = mega_coords[element_idx]
+        node = Node(node_poly, node_coord)
+        nodes.append(node)
+        already_processed_nodes.append(node_coord)
+
+# build the neighbour list based on the presence of the node in a pair or as an intersection
+
+# build the neighbour list based on the presence of the node in a pair
+for node in nodes:
+
+    neighbour_coord_list = []
+
+    # check for the presence of the node in a pair and add the neighbour as the inter point
+    for element_idx in range(len(refined_polytope_pairs)):
+
+        for part in range(0, 2):
+
+            if refined_center_pairs[element_idx][part] == node.coords:
+
+                nb_poly_coord = refined_inters_centers[element_idx]
+
+                if (nb_poly_coord not in neighbour_coord_list) and (nb_poly_coord != node.coords):
+                    nb = [n for n in nodes if n.coords == nb_poly_coord]
+                    node.neighbours.append(nb[0])
+
+                    neighbour_coord_list.append(nb_poly_coord)
+                
+                # nb_poly_coord = refined_center_pairs[element_idx][1 - part]
+
+                # if nb_poly_coord not in neighbour_coord_list:
+                #     # we wanna append the neighbour node objects that have this coordinate
+
+                #     nb = [n for n in nodes if n.coords == nb_poly_coord]
+                #     node.neighbours.append(nb[0])
+
+                #     neighbour_coord_list.append(nb_poly_coord)
+
+# build the neighbour list based on the presence of the node as an intersection
+    for inter_idx in range(len(refined_inters_list)):
+
+        if refined_inters_centers[inter_idx] == node.coords:
+            
+            nb_coord1 = refined_center_pairs[inter_idx][0]
+            nb_coord2 = refined_center_pairs[inter_idx][1]
+
+            if (nb_coord1 not in neighbour_coord_list) and (nb_coord1 != node.coords):
+
+                neighbour_coord_list.append(nb_coord1)
+
+                nb1 = [n for n in nodes if n.coords == nb_coord1]
+                node.neighbours.append(nb1[0])
+
+            if (nb_coord2 not in neighbour_coord_list) and (nb_coord2 != node.coords):
+
+                neighbour_coord_list.append(nb_coord2)
+
+                nb2 = [n for n in nodes if n.coords == nb_coord2]
+                node.neighbours.append(nb2[0])
+
+# Node and neighbour creation has been verified. Time to start doing the Dijkstra processing
 
 
 # Next, we need to assign node-node edge costs
@@ -359,20 +482,11 @@ edge_cost_list = []
 for pair in refined_center_pairs:
     edge_cost = distance(pair[0], pair[1])
     edge_cost_list.append(edge_cost)
-   
-print(f'Length of edge cost list: {len(edge_cost_list)}')
-
-print(refined_center_pairs)
-print(refined_inters_centers)
-
-
-# What data type is the graph????
-
-
-# next we can go on to defining the start and goal nodes and attaching them to the tree
 
 ###############################################################################################
 # RANDOMLY PLACE START AND GOAL NODES
+
+# FIXME: Need to define these as nodes lol
 
 start = [np.random.uniform(x1_min, x1_max), np.random.uniform(x2_min, x2_max)]
 
@@ -384,88 +498,99 @@ goal = [np.random.uniform(x1_min, x1_max), np.random.uniform(x2_min, x2_max)]
 while (goal == start) or (check_obstacle_collision(goal, obstacles) == True) or (distance(start, goal) < (x1_max - x1_min)/2):
     goal = [np.random.uniform(x1_min, x1_max), np.random.uniform(x2_min, x2_max)]
 
-###############################################################################################
-# CONNECT THE START AND GOAL NODE TO THE TREE AND CALCULATE THE EDGE COSTS
- 
-# Connect the start node
-dist = 100 # just a dummy numb er
-for coord in center_list:
-    test_dist = distance(start, coord)
-    if test_dist < dist and check_interpolation([start, coord], obstacles):
-        start_neighbour = coord
-        dist = test_dist
-start_cost = dist
-
-dist = 100
-for coord in center_list:
-    test_dist = distance(goal, coord)
-    if test_dist < dist and check_interpolation([goal, coord], obstacles):
-        goal_neighbour = coord
-        dist = test_dist
-goal_cost = dist
-
-print(start_neighbour)
-print(goal_neighbour)
+# define the start and goal as nodes
+startnode = Node(None, start)
+goalnode = Node(None, goal)
 
 ###############################################################################################
-# BUILD THE PATH
+# DEFINE THE NEIGHBOURS OF THE START AND GOAL NODE
 
-path = [start, start_neighbour]
+for node in nodes:
+    if (check_interpolation([node.coords, start], obstacles) == True) and node.coords != start:
+        startnode.neighbours.append(node)
+        node.neighbours.append(startnode)
 
-curr = start_neighbour
-stuck = False
-goal_found = False
-num_iter = 0
-while curr != goal_neighbour and stuck == False and num_iter < 40:
+for node in nodes:
+    if (check_interpolation([node.coords, goal], obstacles) == True) and node.coords != goal:
+        goalnode.neighbours.append(node)
+        node.neighbours.append(goalnode)
 
-    # check whether curr is in the intersection list or the center pairs list
+# also check for start-goal connection (though the points should not be chosed to let this happen)
+if (check_interpolation([start, goal], obstacles) == True):
+    goalnode.neighbours.append(startnode)
+    startnode.neighbours.append(goalnode)
 
-    
-    ind_list = []
-    candidates = []
-    cost_list = []
-    for connection in refined_center_pairs:
-        if (curr in connection) and (connection[1 - connection.index(curr)] not in path):
-            ind_list.append(refined_center_pairs.index(connection))
-            candidates.append(connection[1 - connection.index(curr)])
-            cost_list.append(edge_cost_list[refined_center_pairs.index(connection)])
-    
-    if ind_list == []:
-        if curr in refined_inters_centers:
-            # get the index of refined_inters_centers
-            neighbours_index = refined_inters_centers.index(curr)
+###############################################################################################
+# RUN DIJKSTRA'S ON THE TREE
 
-            # get the actual neighbour nodes
-            neighbour1 = refined_center_pairs[neighbours_index][0]
-            neighbour2 = refined_center_pairs[neighbours_index][1]
+def run_planner(start, goal):
+    start.seen = True
+    start.cost = 0
+    start.parent = None
+    onDeck = [start]
+    path = []
 
-            # check which is the better neighbour based on cost
-            if distance(curr, neighbour1) <= distance(curr, neighbour2) and neighbour1 not in path:
-                # append this neighbour to the path and set it to current
-                path.append(neighbour1)
-                curr = neighbour1
-            elif neighbour2 not in path:
-                # use the other neighbour
-                path.append(neighbour2)
-                curr = neighbour2
-        else:
-            stuck = True
-            break
-    else:
-        path.append(candidates[np.argmin(cost_list)])
-        curr = candidates[np.argmin(cost_list)]
+    print("Starting Dijkstra's")
 
-    num_iter += 1
+    while True:
 
-    if curr == goal_neighbour:
-        path.append(goal)
+        # check that the deck is not empty
+        if not (len(onDeck) > 0):
+            print('Path not found')
+            return None
+        
+        # Pop the next node (state) from the deck
+        node = onDeck.pop(0)
 
-if goal in path:
-    print(f'Goal Found. Path is: {path}')
-    goal_found = True
+        node.done = True # this means the node has been processed
 
-else:
-    print(f'Goal not found. Incomplete path: {path}')
+        if node.coords == goal.coords:
+            path.append(goal)
+            node_parent = goal.parent
+            total_cost = node.cost
+            # Build the path and calculates the edge costs
+            while node_parent != None:
+
+                # adding the node to the path and calculating edge costs
+                path.append(node_parent)
+                node_parent = node_parent.parent
+
+            path.reverse()
+            print(f'Goal found. Goal cost is: {total_cost}')
+
+            return path
+        
+        elif node == start:
+
+            for element in node.neighbours:
+                element.seen = True
+                element.parent = start
+                element.cost = element.edge_cost(node)
+                bisect.insort(onDeck, element)
+
+        else: # node is neither a start nor goalnode
+            for element in node.neighbours:
+                
+                # check whether the element has been seen (whether a parent has been assigned)
+                if element.seen == False:
+                    element.seen = True
+                    element.parent = node
+                    element.cost = element.edge_cost(node) + node.cost
+                    bisect.insort(onDeck, element)
+
+                # if the element has already been seen (a parent was assigned)
+                elif (element.seen == True) and (element.done == False):
+                    new_cost = element.edge_cost(node) + node.cost
+                    if new_cost < element.cost:
+                        onDeck.remove(element)
+                        element.cost = new_cost
+                        element.parent = node
+                        bisect.insort(onDeck, element)
+                
+
+
+path = run_planner(startnode, goalnode)
+print('Did it work?')
 
 ###############################################################################################
 # PLOTTING
@@ -513,8 +638,8 @@ plt.fill(obs_rect6_pts[0, :], obs_rect6_pts[1, :], 'r')
 
 # plot the Chebyshev centers
 # print('Centers')
-for center in center_list:
-    plt.plot(center[0], center[1], 'bo')
+for pt in mega_coords:
+    plt.plot(pt[0], pt[1], 'bo')
 
 # for point in refined_inters_centers:
 #     plt.plot(point[0], point[1], 'wo')
@@ -586,42 +711,41 @@ for pair in refined_center_pairs:
 
     pair_idx += 1
 
-# plot the path if the goal is found
-length = len(path)
-path_idx = 0
+print(f'Startnode: {startnode.coords}')
 
-while path_idx != length-1:
+for n in startnode.neighbours:
+    print(n.coords)
 
-    inter_exists = False
 
-    # also want to plot the intermediate centers
-    
-    if ([path[path_idx], path[path_idx+1]] in refined_center_pairs):
-        inter_exists = True
-        idx = refined_center_pairs.index([path[path_idx], path[path_idx+1]])
-    elif ([path[path_idx+1], path[path_idx]] in refined_center_pairs):
-        inter_exists = True
-        idx = refined_center_pairs.index([path[path_idx+1], path[path_idx]])
+print(f'Goalnode: {goalnode.coords}')
 
-    if inter_exists == False:
-        x_path = [path[path_idx][0], path[path_idx+1][0]]
-        y_path = [path[path_idx][1], path[path_idx+1][1]]
+for n in goalnode.neighbours:
+    print(n.coords)
 
-        plt.plot(x_path, y_path, 'white')
 
-    else:
-        x_path1 = [path[path_idx][0], refined_inters_centers[idx][0]]
-        x_path2 = [path[path_idx + 1][0], refined_inters_centers[idx][0]]
+for node in nodes:
+    print(f'Node is: {node.coords}')
 
-        y_path1 = [path[path_idx][1], refined_inters_centers[idx][1]]
-        y_path2 = [path[path_idx + 1][1], refined_inters_centers[idx][1]]
+    for n in node.neighbours:
+        print(n.coords)
 
-        plt.plot(x_path1, y_path1, 'white')
-        plt.plot(x_path2, y_path2, 'white')
 
-    path_idx += 1
+if path:
+    path_len = len(path)
+    idx = 0
+    while idx != path_len - 1:
+        x_vals = [path[idx].coords[0], path[idx+1].coords[0]]
+        y_vals = [path[idx].coords[1], path[idx+1].coords[1]]
+        idx += 1
+        plt.plot(x_vals, y_vals, 'white')
+
+    # print the path
+    print('Path')
+    for el in path:
+        print(el.coords)
+
 
 plt.axis('equal')
 plt.show()
 
-
+###### SUMMARY OF DIJKSTRA'S/NODE STUFF
